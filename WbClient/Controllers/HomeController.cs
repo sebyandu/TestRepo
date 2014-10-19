7ad884f31854a8008baa280af8500aa8349e5613
sebyandu <podaru_sebastian@yahoo.com>
2014-10-20 00:19:12 +0300

    Test Proj with ADO DAL

---
+++ WbClient/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using DataLayer;
+
+namespace WbClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+           //var connString= Db.GetConnection;
+            return View();
+        }
+    }
+}

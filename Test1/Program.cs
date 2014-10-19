5be6d316e87d1f6feb98dcd690584d6665928f73
sebyandu <podaru_sebastian@yahoo.com>
2014-10-19 18:24:55 +0300

    Test Project with ADO DAL

---
+++ Test1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Test1
+{
+    [TestFixture]
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            NUnitColorConsole.Main(
+               new[] { Assembly.GetExecutingAssembly().Location }
+               );
+        }
+    }
+}
---
+++ Test1/Reverse.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Core;
+using NUnit.Framework;
+
+namespace Test1
+{
+    static class Operations
+    {
+        public static string Reverse(string input)
+        {
+            input = input.Trim();
+            var length = input.Length;
+            var reverse = string.Empty;
+            for (var i = length - 1; i >= 0; i--)
+            {
+                reverse += input[i];
+            }
+            return reverse;
+        }
+
+        public static bool Palind(string input)
+        {
+            return input.Trim() == Reverse(input);
+        }
+
+        public static List<int> FibonacciArrays(int lastNo)
+        {
+            var result = new List<int>();
+            var i = 1;
+            result.Add(0);
+            result.Add(1);
+            while (lastNo >= result[i])
+            {
+                result.Add(result[i-1]+result[i]);
+                i++;
+            }
+            return result;
+
+        }
+
+        public static int[] SortedArray( int [] inputArray)
+        {
+            for (var i = 0; i < inputArray.Length; i++)
+            {
+                for (var j = 0; j < inputArray.Length-1; j++)
+                {
+                    if (inputArray[j] <= inputArray[j + 1]) continue;
+                    var temp = inputArray[j];
+                    inputArray[j] = inputArray[j + 1];
+                    inputArray[j + 1] = temp;
+                }
+            }
+            return inputArray;
+        }
+    }
+
+
+    [TestFixture]
+    public class OperationsTest
+    {
+        [Test]
+        public void ReverseTest()
+        {
+            Assert.IsTrue(Operations.Reverse("Out") == "tuO");
+        }
+
+        [Test]
+        public void PalindTest()
+        {
+            Assert.IsTrue(Operations.Palind("LUPUL"));
+        }
+
+        [Test]
+        public void FiboTest()
+        {
+            Assert.IsTrue(Operations.FibonacciArrays(8).Capacity-1==7);
+        }
+
+        [Test]
+        public void BubbleSortTest()
+        {
+            Assert.IsTrue(Operations.SortedArray(new[] {3,2,1}).SequenceEqual(new[] {1,2,3}));
+        }
+    }
+}
